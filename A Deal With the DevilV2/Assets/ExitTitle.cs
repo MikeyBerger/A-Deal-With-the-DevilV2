db3a85648afd75d52e13f8a0580b227ef3d8a46e
MikeyBerger <michaeljudahberger@gmail.com>
2021-04-19 10:13:24 -0400

    Final Commit

---
+++ A Deal With the DevilV2/Assets/ExitTitle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitTitle : MonoBehaviour
+{
+    public int NextScene = 1;
+    public float Timer;
+   
+    IEnumerator ChangeScene()
+    {
+        yield return new WaitForSeconds(Timer);
+        SceneManager.LoadScene(NextScene);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ChangeScene());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
